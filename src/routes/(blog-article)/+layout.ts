1c083a250a5d3802b20e4d17f0e28b73fb4df59b
Daniel <danielgrunkin@gmail.com>
2024-08-03 23:50:54 +0200

    trying to make blog posts work

--- src/routes/(blog-article)/+layout.ts
+++ src/routes/(blog-article)/+layout.ts
@@ -2,9 +2,21 @@ import { filteredPosts } from '$lib/data/blog-posts';
 
 export async function load({ url }: { url: { pathname: string } }) {
   const { pathname } = url;
-  const slug = pathname.replace(/\//g, '');
+  console.log(pathname)
+  if (!pathname) {
+    return {
+      post: undefined
+    };
+  }
+  console.log(pathname)
+
+  const slug = pathname.match(/\/([^\/]+)\/?$/)?.[1];
+  if (!slug) {
+    return {
+      post: undefined
+    };
+  }
   const post = filteredPosts.find((post) => post.slug === slug);
-  console.log(slug);
   return {
     post
   };
